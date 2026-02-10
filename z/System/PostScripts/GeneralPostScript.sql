775ad2e494d3b012d603aa42b683853bf3e3f74c
John Huang <7530945+johnhuangca@users.noreply.github.com>
2026-02-09 19:55:40 -0800

    Small changes

--- z/System/PostScripts/GeneralPostScript.sql
+++ z/System/PostScripts/GeneralPostScript.sql
@@ -1,6 +1,7 @@
-exec z.usp_TraceRefreshMetaData
+exec z.usp_TraceRefreshMetaData;
 go
-exec z.usp_DataInitializationMaster
+exec z.usp_DataInitializationMaster;
 go
-
+exec z.usp_ForceNamingConvention;
+go
 drop procedure if exists z.usp_DatabaseBackup
